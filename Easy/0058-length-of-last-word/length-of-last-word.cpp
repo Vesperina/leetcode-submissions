a62a7c947ba09b97d632888fefd2ec65eaa2a053
Vesperina <yashsn87939@gmail.com>
2026-08-03 12:53:30 +0000

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.93%) - LeetX Squads

---
+++ Easy/0058-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size() - 1; 
+        int c = 0; 
+        while(n >= 0 && s[n] == ' '){
+           n--; 
+        }
+        while(n >=0 && s[n] != ' '){ 
+            c++; 
+            n--; 
+        }
+
+    return c; 
+        
+    }
+};
