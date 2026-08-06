654c0ab7f53c737b017aaebcc3be51f368391fea
Vesperina <yashsn87939@gmail.com>
2026-08-06 12:37:10 +0000

    Time: 0 ms (100.00%) | Memory: 11.6 MB (84.01%) - LeetX Squads

---
+++ Easy/0027-remove-element/remove-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int left = 0; 
+        int right = nums.size() - 1; 
+        while(left <= right){ 
+            if(nums[left] != val){ 
+                left++; 
+            }
+            else if(nums[right] == val){ 
+                right--; 
+            }
+            else{ 
+                swap(nums[left],nums[right]); 
+                left++; 
+                right--; 
+            }
+        }
+        return left;
+       
+    }
+};
