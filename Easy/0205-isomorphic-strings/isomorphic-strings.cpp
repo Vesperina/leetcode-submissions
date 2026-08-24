c814c0ea9fa95f299ab016ccf5d5ab3ca7ec09a3
Vesperina <yashsn87939@gmail.com>
2026-08-24 18:37:41 +0000

    Time: 3 ms (39.13%) | Memory: 11.5 MB (7.40%) - LeetX Squads

---
+++ Easy/0205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, vector<int>> mp1;
+        unordered_map<char, vector<int>> mp2; 
+        int n = s.size(); 
+        int m = t.size(); 
+        for(int i = 0; i < n; i++){ 
+            mp1[s[i]].push_back(i); 
+        }
+          for(int i = 0; i < m; i++){ 
+            mp2[t[i]].push_back(i); 
+        }
+        for(int i = 0; i < n; i++){
+            if(mp1[s[i]] != mp2[t[i]]){ 
+                return false; 
+            }
+        }
+        return true;
+        
+    }
+};
